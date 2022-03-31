afdbb11147ebc8b572d22d0dc7ecc33639ef06f2
Christoph Schunk <c.schunk@hzdr.de>
2022-03-31 22:06:36 +0200

    update install script

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ sudo apt update
 sudo apt full-upgrade
 
 echo "Installing prerequisites ..."
+sudo apt-get install git
 sudo apt-get install python3
+sudo apt-get install python3-pip
 sudo apt-get install python3-flask
 sudo apt-get install python3-opencv
 sudo apt-get install python3-picamera
