53f66af8788bd02b558f437d55469615df1a09fa
Christoph Schunk <c.schunk@hzdr.de>
2022-04-25 23:39:22 +0200

    script updated

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-git remote add primarycamera https://192.168.1.10/home/pi/rpimicro
+# git clone pi@192.168.1.10:/home/pi/rpimicro
+
+git remote add primarycamera pi@192.168.1.10:/home/pi/rpimicro
+
 git pull primarycamera
